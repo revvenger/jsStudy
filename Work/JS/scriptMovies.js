1512b6138761f7275a69a111fd3c72dabf1f2e1a
Element <revvvenger@gmail.com>
2022-09-11 22:03:03 +0300

    moviesScript

---
+++ Work/JS/scriptMovies.js
@@ -0,0 +1,15 @@
+const numberOfFilms = +prompt('Скільки фільмів вже подивилися?', '');
+const persnalMovieDataBase = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const lastMovie = prompt('Один з останніх переглянутих фільмів?', '');
+const lastMovieMark = prompt('На скільки балів оцінюєте?', '');
+    persnalMovieDataBase.movies[lastMovie] = lastMovieMark;
+
+console.log(persnalMovieDataBase);
+    
+    
